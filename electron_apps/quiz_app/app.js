e4501d4fc575953ad63a0bd5039e28ae582787b7
Georgi Nikolov <gnikolov@sfu.ca>
2017-10-23 13:34:46 -0600

    Add custom Menu

--- electron_apps/quiz_app/app.js
+++ electron_apps/quiz_app/app.js
@@ -1,8 +1,8 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 const url = require('url')
 
-let win
+let win, quizWin
 
 createWindow = () => {
     // Create Browser Window
@@ -18,19 +18,26 @@ createWindow = () => {
     // Open the DevTools.
     win.webContents.openDevTools()
 
-
      // Emitted when the window is closed.
     win.on('closed', () => {
         // Can store windows in an array if app supports multi windows
         win = null
     })
 
+    // Override pre-build menu with custom menu
+    const customMenu = Menu.buildFromTemplate(menuTemplate)
+    // Set Custom Menu on application launch
+    Menu.setApplicationMenu(customMenu)
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', createWindow)
 
+app.on('activate', () => {
+    // Re-create window for OS X if dock icon is clicked and there are no other windows open
+    if (mainWindow === null) createWindow()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -39,8 +46,36 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
 
+// Create Custom Menu Template
+const menuTemplate = [
+    {
+        label: 'Electron',
+        submenu: [
+            {
+                label: 'Exit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() { app.quit() }
+            },
+        ],
+    },
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Choose Quiz',
+                accelerator: 'Shift+Q',
+                click() { chooseQuiz() }
+            }
+        ],
+    }
+]
 
-app.on('activate', () => {
-    // Re-create window for OS X if dock icon is clicked and there are no other windows open
-    if (mainWindow === null) createWindow()
-})
+chooseQuiz = () => {
+    quizWin = new BrowserWindow({width: 400, height: 300, title: 'Quizes List'})
+
+    quizWin.loadURL(url.format({
+        pathname: path.join(__dirname, 'quizWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+}
